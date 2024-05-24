6edeaba2b11c07706f7447ada34ec6d8b169ce37
Viktor Medvedev <84732735+ViktorM01@users.noreply.github.com>
2024-05-24 11:56:01 +0300

    Create 1528. Shuffle String [Python].py

---
+++ Array/1528. Shuffle String [Python].py
@@ -0,0 +1,6 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        result = [''] * len(s)
+        for i, index in enumerate(indices):
+            result[index] = s[i]
+        return ''.join(result)
