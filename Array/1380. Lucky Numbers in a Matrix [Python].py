0524b17ad523eb788685f3e5fd4c5e5b5f2d37bf
Viktor Medvedev <84732735+ViktorM01@users.noreply.github.com>
2024-07-20 01:34:03 +0300

    Create 1380. Lucky Numbers in a Matrix [Python].py

---
+++ Array/1380. Lucky Numbers in a Matrix [Python].py
@@ -0,0 +1,21 @@
+class Solution:
+    def luckyNumbers(self, matrix):
+        ans = []
+        n = len(matrix)
+        m = len(matrix[0])
+        
+        for i in range(n):
+
+            row_min = min(matrix[i])
+            row_min_index = matrix[i].index(row_min)
+            
+            is_lucky = True
+            for k in range(n):
+                if matrix[k][row_min_index] > row_min:
+                    is_lucky = False
+                    break
+            
+            if is_lucky:
+                ans.append(row_min)
+        
+        return ans
