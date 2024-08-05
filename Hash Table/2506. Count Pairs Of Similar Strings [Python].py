d71de426ebd83110937ca8a202a2c8563d3c49f9
Viktor Medvedev <84732735+ViktorM01@users.noreply.github.com>
2024-08-05 16:22:15 +0300

    Update 2506. Count Pairs Of Similar Strings [Python].py

--- Hash Table/2506. Count Pairs Of Similar Strings [Python].py
+++ Hash Table/2506. Count Pairs Of Similar Strings [Python].py
@@ -1,8 +1,24 @@
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
-        count = 0
-        for i in range(len(words)):
-            for j in range(i + 1, len(words)):
-                if set(words[i]) == set(words[j]):
-                    count += 1
-        return count
+        
+        dct_ = {}
+
+        for n in words:
+            vec = [0] * 26
+            for s in range(len(n)):
+                vec[ord(n[s]) - ord('a')] = 1
+                
+            vec = tuple(vec)
+
+            if vec in dct_:
+                dct_[vec] += 1
+            else:
+                dct_[vec] = 1
+
+        ans = 0
+
+        for key in dct_:
+            k = dct_[key]
+            ans += (k-1) * k // 2
+
+        return ans
