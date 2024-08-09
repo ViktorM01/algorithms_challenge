38123fac0fcd4a2c8a85b40b5c326cccd7b36cff
Viktor Medvedev <84732735+ViktorM01@users.noreply.github.com>
2024-08-09 22:48:22 +0300

    Create D. Быстрый поиск в массиве [Python].py

---
+++ Binary Search/D. Быстрый поиск в массиве [Python].py
@@ -0,0 +1,38 @@
+def lower(x: int, a: []) -> int:
+    l, r = 0, n - 1
+    ans = 0
+    while l <= r:
+        mid = (l + r) // 2
+        if a[mid] >= x:
+            r = mid - 1
+            ans = mid
+        else:
+            l = mid + 1
+    return ans
+ 
+def upper(x: int, a: []) -> int:
+    l, r = 0, n - 1
+    ans = 0
+    while l <= r:
+        mid = (l + r) // 2
+        if a[mid] <= x:
+            l = mid + 1
+            ans = mid
+        else:
+            r = mid - 1
+    return ans
+ 
+ 
+n = int(input())
+a = list(map(int, input().split()))
+k = int(input())
+a.sort()
+ 
+for i in range(k):
+    l, r = map(int, input().split())
+    L = lower(l, a)
+    R = upper(r, a)
+    if a[L] >= l and a[R] <= r:
+        print(R - L + 1, end = ' ')
+    else:
+        print(0, end = ' ')
