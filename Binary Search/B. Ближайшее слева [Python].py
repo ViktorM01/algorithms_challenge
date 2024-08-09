7c6ec1f17fdf063040de24764f26aa8617f7a397
Viktor Medvedev <84732735+ViktorM01@users.noreply.github.com>
2024-08-09 22:47:13 +0300

    Create B. Ближайшее слева [Python].py

---
+++ Binary Search/B. Ближайшее слева [Python].py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+q = list(map(int, input().split()))
+ 
+ 
+for x in q:
+    l, r = 0, n - 1
+    ans = 0
+    while l <= r:
+        mid = (l + r) // 2
+        if a[mid] <= x:
+            l = mid + 1
+            ans = mid
+        else:
+            r = mid - 1
+    if a[0] > x:
+        print(0)
+    else:
+        print(ans+1)
