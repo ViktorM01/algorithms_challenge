6313ec822e29b382fad8dcbe3c36736b6b58f99f
Viktor Medvedev <84732735+ViktorM01@users.noreply.github.com>
2025-02-12 23:57:01 +0300

    Create 2342. Max Sum of a Pair With Equal Sum of Digits [Python].py

---
+++ Hash Table/2342. Max Sum of a Pair With Equal Sum of Digits [Python].py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumSum(self, nums: List[int]) -> int:
+        # possible size just 82 
+        lst = [-1] * 82
+        ans = -1
+
+        for num in nums:
+            digit_sum = sum(int(dd) for dd in str(num))
+            if lst[digit_sum] != 1:
+                ans = max(ans, num + lst[digit_sum])
+            lst[digit_sum] = max(num, lst[digit_sum])
+        return ans
